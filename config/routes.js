7a3660eef6772f69a9e005be7c3beac66a933549
eadpearce <e.pearce@tutanota.com>
2017-05-15 10:53:44 +0100

    Merging with development

--- config/routes.js
+++ config/routes.js
@@ -2,6 +2,11 @@ const router = require('express').Router();
 const widgets = require('../controllers/widgets');
 
 router.route('/widgets')
-  .get(widgets.index);
+  .get(widgets.index)
+  .post(widgets.create);
+router.route('/widgets/:id')
+  .get(widgets.show)
+  .put(widgets.update)
+  .delete(widgets.delete);
 
 module.exports = router;
--- controllers/widgets.js
+++ controllers/widgets.js
@@ -11,7 +11,37 @@ function widgetsIndex(req, res) {
     })
     .catch(err => res.json({ message: err }));
 }
-
+function widgetsShow(req, res) {
+  Widget
+    .findById(req.params.id)
+  .then(widget => res.status(200).json(widget))
+  .catch(err => res.json({ error: err }));
+}
+function widgetsCreate(req, res) {
+  const widget = new Widget(req.body);
+  widget.save(err => {
+    if (err) return res.status(500).json({message: 'Something went wrong.'});
+    return res.status(201).json(widget);
+  });
+}
+function widgetsDelete(req, res) {
+  Widget.findByIdAndRemove(req.params.id, (err, widget) => {
+    if (err) return res.status(500).json({message: 'Something went wrong.'});
+    if (!widget) return res.status(404).json({message: 'No widget was found.'});
+    return res.status(200).json({message: 'Widget successfully deleted'});
+  });
+}
+function widgetsUpdate(req, res) {
+  Widget.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, widget) => {
+    if (err) return res.status(500).json({message: 'Something went wrong.'});
+    if (!widget) return res.status(404).json({message: 'No widget was found.'});
+    return res.status(200).json({message: 'Widget successfully deleted'});
+  });
+}
 module.exports = {
-  index: widgetsIndex
+  index: widgetsIndex,
+  show: widgetsShow,
+  new: widgetsCreate,
+  delete: widgetsDelete,
+  update: widgetsUpdate
 };
