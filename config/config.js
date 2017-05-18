f6771d0224dec9b606e72c077191cfcec96f22a5
patchyj <jackjwmcgregor@gmail.com>
2017-05-18 17:12:05 +0100

    ready to deploy

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 4000,
-  db: 'mongodb://localhost/wdi-group-project',
+  db: process.env.MONGODB_URI || 'mongodb://localhost/wdi-group-project',
   secret: process.env.SECRET || 'silence I kill you.'
 };
